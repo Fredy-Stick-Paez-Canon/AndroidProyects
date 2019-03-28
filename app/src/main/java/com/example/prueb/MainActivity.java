5bc07a0e6fa99d52443c5244a108200498082526
misco <ing.felipepadilla@gmail.com>
2019-03-28 18:06:36 -0500

    Se agrega parte de la funcionalidad en el activity_Main.java

--- app/src/main/java/com/example/prueb/MainActivity.java
+++ app/src/main/java/com/example/prueb/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.prueb;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,15 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
 
     int btnc = 0, band=0;
-    ImageButton boton1 = findViewById(R.id.button1);
-    ImageButton boton2 = findViewById(R.id.button2);
-    ImageButton boton3 = findViewById(R.id.button3);
-    ImageButton boton11 = findViewById(R.id.button11);
-    ImageButton boton22 = findViewById(R.id.button22);
-    ImageButton boton33 = findViewById(R.id.button33);
-    ImageButton boton111 = findViewById(R.id.button111);
-    ImageButton boton222 = findViewById(R.id.button222);
-    ImageButton boton333 = findViewById(R.id.button333);
+
 
 
 
@@ -35,8 +28,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        final ImageButton boton1 = findViewById(R.id.button1);
+        final ImageButton boton2 = findViewById(R.id.button2);
+        ImageButton boton3 = findViewById(R.id.button3);
+        ImageButton boton11 = findViewById(R.id.button11);
+        ImageButton boton22 = findViewById(R.id.button22);
+        ImageButton boton33 = findViewById(R.id.button33);
+        ImageButton boton111 = findViewById(R.id.button111);
+        ImageButton boton222 = findViewById(R.id.button222);
+        ImageButton boton333 = findViewById(R.id.button333);
+/*
         boton1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
@@ -47,87 +48,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+*/
+        boton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                giraImReloj();
+
+              //  boton1.setImageResource(R.drawable.ic_star);
+                boton1.setBackgroundResource(R.mipmap.naranja);
+            }
+        });
+
 
         boton2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                band = 0;
-
                 giraImReloj();
-                cambio_ic(boton2);
+
+                //  boton1.setImageResource(R.drawable.ic_star);
+                boton2.setBackgroundResource(R.mipmap.fresa);
             }
         });
 
-        boton3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                band = 1;
-
-                giraImReloj();
-                cambio_ic(boton3);
-            }
-        });
-
-        boton11.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                band = 0;
-
-                giraImReloj();
-                cambio_ic(boton11);
-            }
-        });
-
-
-        boton22.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                band = 1;
-
-                giraImReloj();
-                cambio_ic(boton22);
-            }
-        });
-
-        boton33.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                band = 0;
-
-                giraImReloj();
-                cambio_ic(boton33);
-            }
-        });
-
-        boton111.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                band = 1;
-
-                giraImReloj();
-                cambio_ic(boton111);
-            }
-        });
-
-        boton222.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                band = 0;
-
-                giraImReloj();
-                cambio_ic(boton222);
-            }
-        });
-
-        boton333.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                band = 1;
-
-                giraImReloj();
-                cambio_ic(boton333);
-            }
-        });
 
 
     }
@@ -146,10 +88,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /*
     protected void cambio_ic(ImageButton btn){
         if(band==1){
 
             btn.setImageResource(R.drawable.ic_star);
+
 
 
         }else{
@@ -159,5 +103,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-
+*/
 }
