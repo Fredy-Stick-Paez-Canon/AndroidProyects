35470dbb6f92ca53c13beb688649cb7f25e35aab
fredy paez <fspaez5@misena.edu.co>
2019-03-28 15:20:45 -0500

    first commit

---
+++ app/src/main/java/com/example/prueb/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.prueb;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton boton1 = findViewById(R.id.button1);
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+              //  boton1.setImageResource(R.drawable.ic_equis);
+
+                Toast toast1 =
+                        Toast.makeText(getApplicationContext(),
+                                "Toast por defecto", Toast.LENGTH_SHORT);
+
+                toast1.show();
+
+                giraImReloj();
+            }
+        });
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    protected void giraImReloj(){
+        ImageView imagenReloj = findViewById(R.id.reloj);
+
+        RotateAnimation animation = new RotateAnimation(0, 360,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+        animation.setDuration(2000);
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.setRepeatMode(Animation.INFINITE);
+        imagenReloj.startAnimation(animation);
+    }
+
+}
